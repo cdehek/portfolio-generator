92d59737c1991a2d941feb77d052055ef61b276f
Christian <christian.dehek@gmail.com>
2020-09-24 16:10:49 -0600

    added function to capture user input and apply it to hmtl file

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+// const [name, github] = profileDataArgs;
 
 // const pageHTML = generatePage(name, github);
-
-// const [name, github] = profileDataArgs;
 
 // fs.writeFile('index.html', pageHTML, err => {
 //     if (err) throw err;
@@ -143,5 +142,11 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('index.html', pageHTML, err => {
+            if (err) throw err;
+
+            console.log('Portfolio complete! Checkout index.html to see the output!')
+        });
     });
